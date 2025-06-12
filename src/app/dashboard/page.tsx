cead01733e3cb1ec6462851f6050104e1ed2b121
Rodrigo Espino <rodrigosantacruz42@gmail.com>
2025-06-12 09:09:22 -0600

    fix router

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,11 @@
+"use client"
 import { useRouter } from 'next/navigation';
-
+import { useEffect } from 'react';
 export default function DashboardPage() {
+  
   const router = useRouter()
   // Redirigir a la página de depósitos
-  router.push('/login');
+useEffect(() => {
+    router.push('/dashboard/deposits'); // Redirige a /login después del renderizado
+  }, [router]);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client"
 import { useRouter } from 'next/navigation';
-
+import { useEffect } from 'react';
 export default function Home() {
   // Redirigir a la página de login
   const router = useRouter();
 
-  router.push('/login');
-
+useEffect(() => {
+    router.push('/login'); // Redirige a /login después del renderizado
+  }, [router]);
 }
